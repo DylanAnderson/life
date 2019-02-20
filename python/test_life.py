051fa52f8ffe7716bf4e6ee9b19244e4bc0fb8a3
Dylan Anderson <dylanz0o4.da@gmail.com>
2019-02-19 22:05:10 -0700

    Grouped tests.

--- python/test_life.py
+++ python/test_life.py
@@ -2,97 +2,95 @@ import numpy as np
 import life
 
 
-def test_block():
-    """Block still life."""
-    pattern = np.array([[0, 0, 0, 0],
-                        [0, 1, 1, 0],
-                        [0, 1, 1, 0],
-                        [0, 0, 0, 0]], dtype=bool)
-    new_pattern = life.model(pattern)
-    np.testing.assert_array_equal(pattern, new_pattern)
+class TestStillLife(object):
+    def test_block(self):
+        """Block still life."""
+        pattern = np.array([[0, 0, 0, 0],
+                            [0, 1, 1, 0],
+                            [0, 1, 1, 0],
+                            [0, 0, 0, 0]], dtype=bool)
+        new_pattern = life.model(pattern)
+        np.testing.assert_array_equal(pattern, new_pattern)
+
+    def test_beehive(self):
+        """Beehive still life."""
+        pattern = np.array([[0, 0, 0, 0, 0, 0],
+                            [0, 0, 1, 1, 0, 0],
+                            [0, 1, 0, 0, 1, 0],
+                            [0, 0, 1, 1, 0, 0],
+                            [0, 0, 0, 0, 0, 0]], dtype=bool)
+        new_pattern = life.model(pattern)
+        np.testing.assert_array_equal(pattern, new_pattern)
+
+    def test_loaf(self):
+        """Loaf still life."""
+        pattern = np.array([[0, 0, 0, 0, 0, 0],
+                            [0, 0, 1, 1, 0, 0],
+                            [0, 1, 0, 0, 1, 0],
+                            [0, 0, 1, 0, 1, 0],
+                            [0, 0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 0, 0]], dtype=bool)
+        new_pattern = life.model(pattern)
+        np.testing.assert_array_equal(pattern, new_pattern)
+
+    def test_boat(self):
+        """Boat still life."""
+        pattern = np.array([[0, 0, 0, 0, 0],
+                            [0, 1, 1, 0, 0],
+                            [0, 1, 0, 1, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 0]], dtype=bool)
+        new_pattern = life.model(pattern)
+        np.testing.assert_array_equal(pattern, new_pattern)
 
 
-def test_beehive():
-    """Beehive still life."""
-    pattern = np.array([[0, 0, 0, 0, 0, 0],
-                        [0, 0, 1, 1, 0, 0],
-                        [0, 1, 0, 0, 1, 0],
-                        [0, 0, 1, 1, 0, 0],
-                        [0, 0, 0, 0, 0, 0]], dtype=bool)
-    new_pattern = life.model(pattern)
-    np.testing.assert_array_equal(pattern, new_pattern)
+class TestOscillators(object):
 
+    def test_blinker(self):
+        """Blinker (period 2)."""
+        pattern_1 = np.array([[0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0],
+                              [0, 1, 1, 1, 0],
+                              [0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0]], dtype=bool)
+        pattern_2 = np.array([[0, 0, 0, 0, 0],
+                              [0, 0, 1, 0, 0],
+                              [0, 0, 1, 0, 0],
+                              [0, 0, 1, 0, 0],
+                              [0, 0, 0, 0, 0]], dtype=bool)
+        np.testing.assert_array_equal(pattern_1, life.model(pattern_2))
+        np.testing.assert_array_equal(pattern_2, life.model(pattern_1))
 
-def test_loaf():
-    """Loaf still life."""
-    pattern = np.array([[0, 0, 0, 0, 0, 0],
-                        [0, 0, 1, 1, 0, 0],
-                        [0, 1, 0, 0, 1, 0],
-                        [0, 0, 1, 0, 1, 0],
-                        [0, 0, 0, 1, 0, 0],
-                        [0, 0, 0, 0, 0, 0]], dtype=bool)
-    new_pattern = life.model(pattern)
-    np.testing.assert_array_equal(pattern, new_pattern)
+    def test_toad(self):
+        """Toad (period 2)."""
+        pattern_1 = np.array([[0, 0, 0, 0, 0, 0],
+                              [0, 0, 0, 1, 0, 0],
+                              [0, 1, 0, 0, 1, 0],
+                              [0, 1, 0, 0, 1, 0],
+                              [0, 0, 1, 0, 0, 0],
+                              [0, 0, 0, 0, 0, 0]], dtype=bool)
+        pattern_2 = np.array([[0, 0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0, 0],
+                              [0, 0, 1, 1, 1, 0],
+                              [0, 1, 1, 1, 0, 0],
+                              [0, 0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0, 0]], dtype=bool)
+        np.testing.assert_array_equal(pattern_1, life.model(pattern_2))
+        np.testing.assert_array_equal(pattern_2, life.model(pattern_1))
 
-
-def test_boat():
-    """Boat still life."""
-    pattern = np.array([[0, 0, 0, 0, 0],
-                        [0, 1, 1, 0, 0],
-                        [0, 1, 0, 1, 0],
-                        [0, 0, 1, 0, 0],
-                        [0, 0, 0, 0, 0]], dtype=bool)
-    new_pattern = life.model(pattern)
-    np.testing.assert_array_equal(pattern, new_pattern)
-
-
-def test_blinker():
-    """Blinker (period 2)."""
-    pattern_1 = np.array([[0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0],
-                          [0, 1, 1, 1, 0],
-                          [0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0]], dtype=bool)
-    pattern_2 = np.array([[0, 0, 0, 0, 0],
-                          [0, 0, 1, 0, 0],
-                          [0, 0, 1, 0, 0],
-                          [0, 0, 1, 0, 0],
-                          [0, 0, 0, 0, 0]], dtype=bool)
-    np.testing.assert_array_equal(pattern_1, life.model(pattern_2))
-    np.testing.assert_array_equal(pattern_2, life.model(pattern_1))
-
-
-def test_toad():
-    """Toad (period 2)."""
-    pattern_1 = np.array([[0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 1, 0, 0],
-                          [0, 1, 0, 0, 1, 0],
-                          [0, 1, 0, 0, 1, 0],
-                          [0, 0, 1, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0]], dtype=bool)
-    pattern_2 = np.array([[0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0],
-                          [0, 0, 1, 1, 1, 0],
-                          [0, 1, 1, 1, 0, 0],
-                          [0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0]], dtype=bool)
-    np.testing.assert_array_equal(pattern_1, life.model(pattern_2))
-    np.testing.assert_array_equal(pattern_2, life.model(pattern_1))
-
-
-def test_beacon():
-    """Beacon (period 2)."""
-    pattern_1 = np.array([[0, 0, 0, 0, 0, 0],
-                          [0, 1, 1, 0, 0, 0],
-                          [0, 1, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 1, 0],
-                          [0, 0, 0, 1, 1, 0],
-                          [0, 0, 0, 0, 0, 0]], dtype=bool)
-    pattern_2 = np.array([[0, 0, 0, 0, 0, 0],
-                          [0, 1, 1, 0, 0, 0],
-                          [0, 1, 1, 0, 0, 0],
-                          [0, 0, 0, 1, 1, 0],
-                          [0, 0, 0, 1, 1, 0],
-                          [0, 0, 0, 0, 0, 0]], dtype=bool)
-    np.testing.assert_array_equal(pattern_1, life.model(pattern_2))
-    np.testing.assert_array_equal(pattern_2, life.model(pattern_1))
+    def test_beacon(self):
+        """Beacon (period 2)."""
+        pattern_1 = np.array([[0, 0, 0, 0, 0, 0],
+                              [0, 1, 1, 0, 0, 0],
+                              [0, 1, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 1, 0],
+                              [0, 0, 0, 1, 1, 0],
+                              [0, 0, 0, 0, 0, 0]], dtype=bool)
+        pattern_2 = np.array([[0, 0, 0, 0, 0, 0],
+                              [0, 1, 1, 0, 0, 0],
+                              [0, 1, 1, 0, 0, 0],
+                              [0, 0, 0, 1, 1, 0],
+                              [0, 0, 0, 1, 1, 0],
+                              [0, 0, 0, 0, 0, 0]], dtype=bool)
+        np.testing.assert_array_equal(pattern_1, life.model(pattern_2))
+        np.testing.assert_array_equal(pattern_2, life.model(pattern_1))
