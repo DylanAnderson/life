3cf8b086e1714b507fb58544d240169c6377b555
Dylan Anderson <dylanz0o4.da@gmail.com>
2019-02-19 22:30:29 -0700

    Added series of single cell tests.

--- python/test_life.py
+++ python/test_life.py
@@ -2,6 +2,106 @@ import numpy as np
 import life
 
 
+class TestCell(object):
+
+    def combos(self, p):
+        return (life.cell(p),
+                life.cell(np.flipud(p)),
+                life.cell(np.fliplr(p)),
+                life.cell(np.flipud(np.fliplr(p))))
+
+    def test_corner(self):
+        # No rule - dead
+        p = np.array([[1, 0, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]])
+        assert not any(self.combos(p))
+
+    def test_stack(self):
+        # No rule - dead
+        p = np.array([[1, 1, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]])
+        assert not any(self.combos(p))
+
+    def test_row(self):
+        # Rule 4
+        p = np.array([[1, 1, 1],
+                      [0, 0, 0],
+                      [0, 0, 0]])
+        assert all(self.combos(p))
+
+    def test_short_L(self):
+        # No rule - dead
+        p = np.array([[1, 1, 1],
+                      [1, 0, 0],
+                      [0, 0, 0]])
+        assert not any(self.combos(p))
+
+    def test_long_L(self):
+        # No rule - dead
+        p = np.array([[1, 1, 1],
+                      [1, 0, 0],
+                      [1, 0, 0]])
+        assert not any(self.combos(p))
+
+    def test_G(self):
+        # No rule - dead
+        p = np.array([[1, 1, 1],
+                      [1, 0, 0],
+                      [1, 1, 0]])
+        assert not any(self.combos(p))
+
+    def test_U(self):
+        # No rule - dead
+        p = np.array([[1, 1, 1],
+                      [1, 0, 0],
+                      [1, 1, 1]])
+        assert not any(self.combos(p))
+
+    def test_donut(self):
+        # No rule - dead
+        p = np.array([[1, 1, 1],
+                      [1, 0, 1],
+                      [1, 1, 1]])
+        assert not any(self.combos(p))
+
+    def test_lone_survivor(self):
+        # Rule 1: dead
+        p = np.array([[0, 0, 0],
+                      [0, 1, 0],
+                      [0, 0, 0]])
+        assert not any(self.combos(p))
+
+    def test_duo(self):
+        # Rule 1: dead
+        p = np.array([[1, 0, 0],
+                      [0, 1, 0],
+                      [0, 0, 0]])
+        assert not any(self.combos(p))
+
+    def test_triangle(self):
+        # Rule 2: alive
+        p = np.array([[1, 0, 1],
+                      [0, 1, 0],
+                      [0, 0, 0]])
+        assert all(self.combos(p))
+
+    def test_squads(self):
+        # Rule 2: alive
+        p = np.array([[1, 0, 1],
+                      [0, 1, 0],
+                      [0, 1, 0]])
+        assert all(self.combos(p))
+
+    def test_fives_company(self):
+        # Rule 3: dead
+        p = np.array([[1, 0, 1],
+                      [0, 1, 0],
+                      [1, 1, 0]])
+        assert not any(self.combos(p))
+
+
 class TestStillLife(object):
     def test_block(self):
         """Block still life."""
